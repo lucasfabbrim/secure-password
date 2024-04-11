7ec822dd14dbc7bdf0e1a366cda7ba7f03a66f07
Lucas Mendes <lucasmendescontact@gmail.com>
2024-04-11 16:16:54 -0300

    feat: rearranging

--- src/main/java/br/com/lucas/modules/password/controller/PasswordController.java
+++ src/main/java/br/com/lucas/modules/password/controller/PasswordController.java
@@ -1,19 +1,1 @@
-package br.com.lucas.modules.password.controller;
-
-import br.com.lucas.modules.password.entities.Password;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("")
-public class PasswordController {
-
-    @PostMapping("/validate-password")
-    public ResponseEntity<Password> getPassword(){
-        return null;
-    }
-    
-}
